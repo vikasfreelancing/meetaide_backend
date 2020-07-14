d82432a7c8c8863b37d2be11652271a924351e9d
vikas vashistha <vikas.vashistha@paytm.com>
2020-07-14 20:29:47 +0530

    initial commit

---
+++ src/main/java/com/meetaide/meetaide/MeetaideApplication.java
@@ -0,0 +1,13 @@
+package com.meetaide.meetaide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetaideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetaideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetaide/meetaide/MeetaideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetaide.meetaide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetaideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

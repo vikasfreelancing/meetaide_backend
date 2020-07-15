a829e8334b05a593b46bcc6c54def2aac262ec89
vikas vashistha <vikas.vashistha@paytm.com>
2020-07-15 15:16:09 +0530

    adding mailchimp api

--- src/main/java/com/meetaide/meetaide/service/MailchimpService.java
+++ src/main/java/com/meetaide/meetaide/service/MailchimpService.java
@@ -2,10 +2,8 @@ package com.meetaide.meetaide.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.meetaide.meetaide.constants.EmailConstants;
 import com.meetaide.meetaide.constants.MailchimpConstants;
 import com.meetaide.meetaide.dto.GetCompainsDto;
-import com.microtripit.mandrillapp.lutung.MandrillApi;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,7 +11,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -29,22 +26,23 @@ public class MailchimpService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         //1519e7e94f566dd55f584afc519812ba-a83a87a9-dedf987c
         headers.setBasicAuth("anystring", MailchimpConstants.API_KEY);
-        HttpEntity requestEntity = new HttpEntity<>( headers);
-        ResponseEntity<String> response = restTemplate.exchange(MailchimpConstants.GET_CAMP, HttpMethod.GET,requestEntity,String.class);
+        HttpEntity requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(MailchimpConstants.GET_CAMP, HttpMethod.GET, requestEntity, String.class);
         System.out.println(response);
-        GetCompainsDto compainsDto =obj.readValue(response.getBody(),GetCompainsDto.class);
+        GetCompainsDto compainsDto = obj.readValue(response.getBody(), GetCompainsDto.class);
         compainsDto.getCampaigns().forEach(campaign -> {
             String compaignId = campaign.getId();
             String listId = campaign.getRecipients().getListId();
-            if(!Strings.isEmpty(listId)&&!Strings.isBlank(listId)){
-                Map<String,Object> postBody = new HashMap<>();
+            if (!Strings.isEmpty(listId) && !Strings.isBlank(listId)) {
+                Map<String, Object> postBody = new HashMap<>();
                 postBody.put("test_emails", Arrays.asList(email));
-                postBody.put("send_type","plain_text");
-                HttpEntity<Map<String,Object>>post = new HttpEntity<>( postBody,headers);
-                String sendEmail = MailchimpConstants.GET_CAMP+"/"+compaignId+"/actions/test";
+                postBody.put("send_type", "plain_text");
+                HttpEntity<Map<String, Object>> post = new HttpEntity<>(postBody, headers);
+                String sendEmail = MailchimpConstants.GET_CAMP + "/" + compaignId + "/actions/test";
                 System.out.println(sendEmail);
-                ResponseEntity<String> res = restTemplate.exchange(sendEmail, HttpMethod.POST,post,String.class);
+                ResponseEntity<String> res = restTemplate.exchange(sendEmail, HttpMethod.POST, post, String.class);
                 System.out.println(res);
+                return;
             }
         });
     }
